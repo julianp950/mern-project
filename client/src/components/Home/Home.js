0a4c82e1ea4642ee05be296071b4b8b516d1b1af
julianp950 <julian.p950@gmail.com>
2022-06-24 14:26:34 -0300

    Initial commit

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import { Container, Grow, Grid, Paper } from "@material-ui/core";
+import Posts from '../Posts/Posts'
+import Form from '../Form/Form';
+import { useDispatch } from "react-redux";
+import { getPosts } from "../../actions/posts";
+import useStyles from '../../styles';
+import Pagination from '../Pagination';
+
+const Home = () => {
+    const [currentId, setCurrentId] = useState(null);
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [currentId, dispatch]);
+
+    return (
+        <Grow in>
+            <Container>
+                <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts setCurrentId={setCurrentId} />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                        <Paper className={classes.pagination} elevation={6}>
+                            <Pagination />
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+    );
+}
+
+export default Home;
